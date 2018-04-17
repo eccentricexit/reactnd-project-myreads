076a228710aa25587630306250474c526e80aa33
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-04-16 22:01:31 -0300

    feat: Add basic components and navigation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// TODO: README.md says don't modify this file, but we need BrowserRouter.
+// TODO: README.md says we shouldn't need to modify this, but I needed react-dom-router
+// Ask for alternatives
+
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root')
+)
